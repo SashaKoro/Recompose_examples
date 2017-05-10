1a4ef46d05856659f099037fc183854d4bfb784f
sashakoro <balaskorotkov@gmail.com>
2017-05-10 08:23:46 -0400

    improved

--- src/Post.js
+++ src/Post.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import compose from 'recompose/compose';
 import setDisplayName from 'recompose/setDisplayName';
@@ -14,6 +16,6 @@ export const Post = ({ title, content, footer }) => {
 };
 
 export default compose(
-  // setDisplayName('WrappedPost'),
-  // renameProp('title', 'footer')
+  setDisplayName('WrappedPost'),
+  renameProp('title', 'footer')
 )(Post);
